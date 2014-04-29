0ee07c8dfff517f6b143ea9517b9ce9d9f435ebb
Hannes Holton <hannes@holtonfamily.net>
2014-04-29 15:59:10 +1000

    Removed a Micorposts resource

--- app/views/microposts/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@microposts) do |micropost|
-  json.extract! micropost, :content, :fk_user_id
-  json.url micropost_url(micropost, format: :json)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429052518) do
-
-  create_table "microposts", force: true do |t|
-    t.string   "content"
-    t.integer  "fk_user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140429042619) do
 
   create_table "users", force: true do |t|
     t.string   "name"
